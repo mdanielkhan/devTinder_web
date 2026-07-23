8b10c58c4db96b5ec4d086e1649f9e15ce6d298f
Daniyal Khan <mdanielkhan2@gmail.com>
2026-07-23 17:59:36 +0500

    socket for deployment

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,7 +2,9 @@ import { BASE_URL } from "./contants"
 import io from "socket.io-client"
 
 export const createSocketConnection = () => {
-  return io(BASE_URL, {
-    withCredentials: true,
-  });
+  if (location.hostname === "localhost") {
+    return io(BASE_URL);
+  } else {
+    return io("/", { path: "/api/socket.io" });
+  }
 };
